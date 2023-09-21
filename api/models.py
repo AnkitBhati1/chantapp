d23942a8a0d58a08a08781ac675cc59da633a606
AnkitBhati1 <bhatiankit9460@gmail.com>
2023-09-21 21:05:37 +0530

    first commit 8

--- api/models.py
+++ api/models.py
@@ -10,3 +10,12 @@ class chatRoom(models.Model):
 
     def __str__(self):
         return self.name
+    
+class message(models.Model):
+    chatroom = models.ForeignKey(chatRoom, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.content
